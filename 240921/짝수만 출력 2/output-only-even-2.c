d79e359013b40fddd11313e95cedf240e00965cb
taayye4 <tayekim4@gmail.com>
2024-09-21 18:24:24 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240921/짝수만 출력 2/output-only-even-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    while(a >= b)
+    {
+        if(a % 2 == 0)
+            printf("%d ", a);
+        a--;
+    }
+    return 0;
+}
