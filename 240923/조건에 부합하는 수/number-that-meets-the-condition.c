3956bc1e60edcfc42fd0a6283d15ac0f19852815
taayye4 <tayekim4@gmail.com>
2024-09-23 14:18:09 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    
+    for(int i = 1; i <= a; i++)
+    {
+        if(i % 2 == 0 && i % 4 != 0)
+            continue;
+        if((i / 8) % 2 == 0)
+            continue;
+        if((i % 7) < 4)
+            continue; 
+        printf("%d ", i);
+    }
+    return 0;
+}
